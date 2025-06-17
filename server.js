53c5b0bdab4b1e2604dec1e11318489b0232cf90
Paras Shah <paras178a@gmail.com>
2025-06-17 17:22:15 +0530

    Add capability to generate QR Code on server side
    Add a filler on website about its working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import qr from "qr-image";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -9,6 +10,15 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
+
+app.get("/generate", (req, res) => {
+    const url = req.query.url;
+    if(!url) return res.status(400).send("Missing URL");
+    
+    res.setHeader("Content-Type", "image/png");
+    const qr_png = qr.image(url, {type: "png", size: 10});
+    qr_png.pipe(res);
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
